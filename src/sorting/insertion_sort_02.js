50be3f6fdc27161ab310f1804d9a717dc8aaa0da
Antonio Diaz <antonio.diaz.arroyo@gmail.com>
2018-03-16 17:10:39 +0000

    insertion sort 02

---
+++ src/sorting/insertion_sort_02.js
@@ -0,0 +1,16 @@
+var insertionSort02 = (arr) => {
+    if (arr.length>=1) {
+        for (var i=1;i<arr.length;i++) {
+            for (var j = i; j >=0; j--) {
+                if (arr[j-1]>arr[j]) {
+                    var temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+    return arr;
+};
+
+exports.insertionSort02 = insertionSort02;
---
+++ test/sorting/insertion_sort_02_test.js
@@ -0,0 +1,11 @@
+var assert = require("chai").assert;
+var hackerankChallenge = require("../../src/sorting/insertion_sort_02.js");
+
+describe("sorting > insertion sort 02: ", function() {
+    var inputArray = "1 4 3 5 6 2".split(" ").map(x => Number(x));
+    it("input 01: " + inputArray, function() {
+        var sortedArray = hackerankChallenge.insertionSort02(inputArray);
+        assert.equal(6, sortedArray.length);
+        assert.equal(2, sortedArray[1]);
+    });
+});
